9a1a0cced626164e5bb46ae6adf1829cc3fe6c8f
deedeedextor <zavinagi_15@abv.bg>
2019-11-03 11:46:06 +0200

    Added CommandUI and Validation classes

--- C#Databases/Entity Framework Core - 10.2019/Code First Model/CodeFirstModel/P01_HospitalDatabase/Data/Configuration.cs
+++ C#Databases/Entity Framework Core - 10.2019/Code First Model/CodeFirstModel/P01_HospitalDatabase/Data/Configuration.cs
@@ -1,9 +1,5 @@
 ﻿namespace P01_HospitalDatabase.Data
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-
     public class Configuration
     {
         public const string ConnectionString = @"Server=;Database=;";
