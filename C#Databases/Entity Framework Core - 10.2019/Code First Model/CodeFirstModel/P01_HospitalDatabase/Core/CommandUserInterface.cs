712709b09117453787067c9862ee69f57e6d4efd
deedeedextor <zavinagi_15@abv.bg>
2019-11-03 11:54:08 +0200

    Added Class CommandUI and Validation

---
+++ C#Databases/Entity Framework Core - 10.2019/Code First Model/CodeFirstModel/P01_HospitalDatabase/Core/CommandUserInterface.cs
@@ -0,0 +1,152 @@
+﻿namespace P01_HospitalDatabase.Core
+{
+    using P01_HospitalDatabase.Data;
+    using P01_HospitalDatabase.Data.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class CommandUserInterface
+    {
+        Validation validator = new Validation();
+
+        public void Register(HospitalContext context)
+        {
+            Console.WriteLine("Please, register!");
+
+            List<string> doctorData = this.InputDoctorData();
+
+            Doctor doctor = new Doctor
+            {
+                Name = doctorData[0],
+                Specialty = doctorData[1],
+                Email = doctorData[2],
+                Password = doctorData[3]
+            };
+
+            context.Doctors.Add(doctor);
+            context.SaveChanges();
+
+            this.Login(context);
+        }
+
+        public void Login(HospitalContext context)
+        {
+            Console.WriteLine("Please, login!");
+            Console.Write("Please, enter your email: ");
+            string emailLogin = Console.ReadLine();
+
+            var email = context
+                .Doctors
+                .Where(e => e.Email == emailLogin)
+                .Select(d => d.Email)
+                .ToList();
+
+            if (email.Any())
+            {
+                Console.WriteLine("Please, enter your password: ");
+                string passwordLogin = Console.ReadLine();
+
+                var password = context
+                    .Doctors
+                    .Where(p => p.Password == passwordLogin)
+                    .Select(p => p.Password)
+                    .ToList();
+
+                if (password.Any())
+                {
+                    var doctor = context
+                        .Doctors
+                        .Select(d => new
+                        {
+                            d.Name,
+                            d.Specialty,
+                            d.Email
+                        })
+                        .FirstOrDefault(d => d.Email == emailLogin);
+
+                    Console.WriteLine($"Welcome, Dr. {doctor.Name} with {doctor.Specialty}");
+                    Console.Write($"Please, choose C to Create Patient, S to Select Patient or E to Edit Patient: ");
+                    string answer = Console.ReadLine();
+
+                    if (answer.ToUpper() == "C")
+                    {
+                        this.CreatePatient(context);
+                    }
+
+                    else if (answer.ToUpper() == "S")
+                    {
+                        this.SelectPatient(context);
+                    }
+
+                    else if (answer.ToUpper() == "E")
+                    {
+                        this.EditPatient(context);
+                    }
+
+                    else
+                    {
+                        validator.ValidateAnswer();
+                    }
+                }
+
+                else
+                {
+                    validator.ValidatePassword();
+                }
+            }
+
+            else
+            {
+                validator.ValidateEmail();
+            }
+        }
+
+        private void EditPatient(HospitalContext context)
+        {
+            //TO DO
+            throw new NotImplementedException();
+        }
+
+        private void SelectPatient(HospitalContext context)
+        {
+            //TO DO
+            throw new NotImplementedException();
+        }
+
+        private void CreatePatient(HospitalContext context)
+        {
+            //TO DO
+            throw new NotImplementedException();
+        }
+
+        private List<string> InputDoctorData()
+        {
+            List<string> data = new List<string>();
+
+            Console.Write("Please, enter your names: ");
+            string name = Console.ReadLine();
+            data.Add(name);
+
+            Console.Write("Please, enter your specialty: ");
+            string specialty = Console.ReadLine();
+            data.Add(specialty);
+
+            Console.Write("Please, enter your email: ");
+            string email = Console.ReadLine();
+            data.Add(email);
+
+            Console.Write("Please, enter your password: ");
+            string password = Console.ReadLine();
+            data.Add(password);
+
+            return data;
+        }
+
+        private List<string> InputPatientData()
+        {
+            //TO DO
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#Databases/Entity Framework Core - 10.2019/Code First Model/CodeFirstModel/P01_HospitalDatabase/Validation.cs
@@ -0,0 +1,27 @@
+﻿namespace P01_HospitalDatabase
+{
+    using System;
+
+    public class Validation
+    {
+        public string ValidateAnswer()
+        {
+            throw new ArgumentException("Incorrect answer!");
+        }
+
+        public string ValidatePassword()
+        {
+            throw new ArgumentException("Incorrect password!");
+        }
+
+        public string ValidateEmail()
+        {
+            throw new ArgumentException("Incorrect email!");
+        }
+
+        public string ValidateName()
+        {
+            throw new ArgumentException("Incorrect name!");
+        }
+    }
+}
