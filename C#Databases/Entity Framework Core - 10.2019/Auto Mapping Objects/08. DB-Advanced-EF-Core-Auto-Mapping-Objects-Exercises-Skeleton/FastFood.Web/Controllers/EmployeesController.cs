649902a08ed0452041aff7f9bb37372027eb8274
deedeedextor <zavinagi_15@abv.bg>
2019-11-10 09:41:09 +0200

    Added auto mapping objects exercise

---
+++ C#Databases/Entity Framework Core - 10.2019/Auto Mapping Objects/08. DB-Advanced-EF-Core-Auto-Mapping-Objects-Exercises-Skeleton/FastFood.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,59 @@
+﻿namespace FastFood.Web.Controllers
+{
+    using AutoMapper;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+
+    using Data;
+    using ViewModels.Employees;
+    using AutoMapper.QueryableExtensions;
+    using System.Linq;
+    using FastFood.Models;
+
+    public class EmployeesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public EmployeesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Register()
+        {
+            var positions = this.context
+                .Positions
+                .ProjectTo<RegisterEmployeeViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(positions);
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterEmployeeInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var employee = this.mapper.Map<Employee>(model);
+
+            this.context.Employees.Add(employee);
+            context.SaveChanges();
+
+            return RedirectToAction("All", "Employees");
+        }
+
+        public IActionResult All()
+        {
+            var employees = this.context.Employees
+                .ProjectTo<EmployeesAllViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(employees);
+        }
+    }
+}
---
+++ C#Databases/Entity Framework Core - 10.2019/Auto Mapping Objects/08. DB-Advanced-EF-Core-Auto-Mapping-Objects-Exercises-Skeleton/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,37 @@
+﻿namespace FastFood.Web.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Web.ViewModels.Categories;
+    using FastFood.Web.ViewModels.Employees;
+    using Models;
+
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Employee
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(p => p.Id));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(p => p.Position.Name));
+
+            //Category
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(c => c.CategoryName));
+
+
+        }
+    }
+}
