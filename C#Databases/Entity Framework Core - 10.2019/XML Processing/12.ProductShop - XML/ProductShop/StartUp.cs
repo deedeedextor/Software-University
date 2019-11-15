55039fdb48e1025395376d6dbab0021b9f99fac1
deedeedextor <zavinagi_15@abv.bg>
2019-11-15 14:11:59 +0200

    Added the import exercises

--- C#Databases/Entity Framework Core - 10.2019/XML Processing/12.ProductShop - XML/ProductShop/StartUp.cs
+++ C#Databases/Entity Framework Core - 10.2019/XML Processing/12.ProductShop - XML/ProductShop/StartUp.cs
@@ -1,10 +1,116 @@
-﻿namespace ProductShop
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<ProductShopProfile>();
+            });
 
+            var context = new ProductShopContext();
+
+            //context.Database.EnsureCreated();
+
+            string inputXml = File.ReadAllText(@"C:\Users\Diana\Desktop\Software-University\C#Databases\Entity Framework Core - 10.2019\XML Processing\12.ProductShop - XML\ProductShop\Datasets\categories-products.xml");
+
+            Console.WriteLine(ImportCategoryProducts(context, inputXml));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportUserDto[]), new XmlRootAttribute("Users"));
+
+            var usersDto = (ImportUserDto[])xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            var users = new List<User>();
+
+            foreach (var userDto in usersDto)
+            {
+                var user = Mapper.Map<User>(userDto);
+                users.Add(user);
+            }
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportProductDto[]), new XmlRootAttribute("Products"));
+
+            var productsDto = (ImportProductDto[])xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            var products = new List<Product>();
+
+            foreach (var productDto in productsDto)
+            {
+                var product = Mapper.Map<Product>(productDto);
+                products.Add(product);
+            }
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportCategoryDto[]), new XmlRootAttribute("Categories"));
+
+            var categoriesDto = (ImportCategoryDto[])xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            var categories = new List<Category>();
+
+            foreach (var categoryDto in categoriesDto)
+            {
+                var category = Mapper.Map<Category>(categoryDto);
+                categories.Add(category);
+            }
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportCategoryProductDto[]), new XmlRootAttribute("CategoryProducts"));
+
+            var categoriesProductsDto = (ImportCategoryProductDto[])xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            var categoryProducts = new List<CategoryProduct>();
+
+            foreach (var categoryProductDto in categoriesProductsDto)
+            {
+                var targetCategory = context.Categories.Find(categoryProductDto.CategoryId);
+                var targetProduct = context.Products.Find(categoryProductDto.ProductId);
+
+                if (targetCategory != null && targetProduct != null)
+                {
+                    var categoryProduct = Mapper.Map<CategoryProduct>(categoryProductDto);
+                    categoryProducts.Add(categoryProduct);
+                }
+            }
+
+            context.CategoryProducts.AddRange(categoryProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoryProducts.Count}";
         }
     }
 }
