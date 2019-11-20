fbed63d908e12700d767aae516d90e3729590c7a
deedeedextor <zavinagi_15@abv.bg>
2019-11-20 17:00:18 +0200

    Updated SongsAboveGivenDuration method

---
+++ C#Databases/Entity Framework Core - 10.2019/Exams/18.04.2019/01. Model Defition_Skeleton + Datasets/MusicHub/DataProcessor/ExportDtos/ExportAlbumDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MusicHub.DataProcessor.ExportDtos
+{
+    public class ExportAlbumDto
+    {
+        public string AlbumName { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string ProducerName { get; set; }
+
+        public ICollection<ExportSongForAlbumDto> Songs { get; set; }
+
+        public string AlbumPrice { get; set; }
+    }
+}
--- C#Databases/Entity Framework Core - 10.2019/Exams/18.04.2019/01. Model Defition_Skeleton + Datasets/MusicHub/DataProcessor/Serializer.cs
+++ C#Databases/Entity Framework Core - 10.2019/Exams/18.04.2019/01. Model Defition_Skeleton + Datasets/MusicHub/DataProcessor/Serializer.cs
@@ -1,21 +1,79 @@
 ﻿namespace MusicHub.DataProcessor
 {
     using System;
-
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
     using Data;
+    using MusicHub.DataProcessor.ExportDtos;
+    using Newtonsoft.Json;
 
     public class Serializer
     {
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            /*The given method in the project skeleton receives a producer id.Export all albums which are produced by the provided producer id.For each album, get the name, release date in format "MM/dd/yyyy", producer name, the album songs with each song name, price formatted to the second digit and the song writer name. Sort the songs by song name(descending) and by writer(ascending).At the end export the total album price with exactly two digits after the decimal place. Sort the albums by their price(descending).*/
+            var albums = context.Albums
+                .Where(a => a.ProducerId == producerId)
+                .Select(a => new ExportAlbumDto
+                {
+                    AlbumName = a.Name,
+                    ReleaseDate = a.ReleaseDate.ToString("MM/dd/yyyy"),
+                    ProducerName = a.Producer.Name,
+                    Songs = a.Songs.Select(s => new ExportSongForAlbumDto
+                    {
+                        SongName = s.Name,
+                        Price = s.Price.ToString("F2"),
+                        Writer = s.Writer.Name
+                    })
+                    .OrderByDescending(s => s.SongName)
+                    .ThenBy(s => s.Writer)
+                    .ToList(),
+                    AlbumPrice = a.Songs.Sum(p => p.Price).ToString("F2")
+                })
+                .OrderByDescending(a => a.AlbumPrice)
+                .ToList();
 
-            throw new NotImplementedException();
+            var json = JsonConvert.SerializeObject(albums, new JsonSerializerSettings
+            {
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+            });
+
+            return json;
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
-            throw new NotImplementedException();
+            var songs = context.Songs
+                .Where(s => s.Duration.TotalSeconds > duration)
+                .Select(s => new ExportSongDto
+                {
+                    SongName = s.Name,
+                    Performer = s.SongPerformers
+                                 .Select(sp => sp.Performer.FirstName + " " + sp.Performer.LastName)
+                                 .FirstOrDefault(),
+                    Writer = s.Writer.Name,
+                    AlbumProducer = s.Album.Producer.Name,
+                    Duration = s.Duration.ToString(@"hh\:mm\:ss")
+                })
+                .OrderBy(s => s.SongName)
+                .ThenBy(s => s.Writer)
+                .ThenBy(s => s.Performer)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportSongDto[]), new XmlRootAttribute("Songs"));
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces(new[]
+            {
+                new XmlQualifiedName(),
+            });
+
+            xmlSerializer.Serialize(new StringWriter(sb), songs, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
