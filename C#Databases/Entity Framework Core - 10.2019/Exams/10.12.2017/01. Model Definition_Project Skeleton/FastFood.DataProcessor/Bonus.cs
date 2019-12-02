420ac8bdc0e874c5e5d54e8bedd8d95459b66a27
deedeedextor <zavinagi_15@abv.bg>
2019-12-02 19:50:19 +0200

    Added bonus task - Update Price

--- C#Databases/Entity Framework Core - 10.2019/Exams/10.12.2017/01. Model Definition_Project Skeleton/FastFood.DataProcessor/Bonus.cs
+++ C#Databases/Entity Framework Core - 10.2019/Exams/10.12.2017/01. Model Definition_Project Skeleton/FastFood.DataProcessor/Bonus.cs
@@ -1,13 +1,28 @@
 ﻿namespace FastFood.DataProcessor
 {
-    using System;
+    using System.Linq;
     using FastFood.Data;
 
     public static class Bonus
     {
 	    public static string UpdatePrice(FastFoodDbContext context, string itemName, decimal newPrice)
 	    {
-			throw new NotImplementedException();
+            var item = context.Items
+                .SingleOrDefault(i => i.Name == itemName);
+
+            if (item == null)
+            {
+                return $"Item {itemName} not found!";
+            }
+
+            var oldPrice = item.Price;
+
+            item.Price = newPrice;
+            context.Items.Update(item);
+
+            context.SaveChanges();
+
+            return $"{item.Name} Price updated from ${oldPrice:F2} to ${newPrice:F2}";
 	    }
     }
 }
