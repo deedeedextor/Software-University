300a8631a831a0934acef1e4b466bcd54e83a942
deedeedextor <zavinagi_15@abv.bg>
2019-12-16 18:23:17 +0200

    Added SIS Exercise - Response class to be implemented

---
+++ C# Web/Web Basics/SIS/SIS/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Exceptions
+{
+    public class InternalServerErrorException : Exception
+    {
+        private const string InternalServerErrorDefaultMessage = "The Server has encountered an error.";
+
+        public InternalServerErrorException()
+            :this(InternalServerErrorDefaultMessage)
+        {
+        }
+
+        public InternalServerErrorException(string name )
+            : base(name)
+        {
+        }
+    }
+}
---
+++ C# Web/Web Basics/SIS/SIS/Requests/HttpRequest.cs
@@ -0,0 +1,138 @@
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Exceptions;
+using SIS.HTTP.Headers;
+using SIS.HTTP.Headers.Contracts;
+using SIS.HTTP.Requests.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIS.HTTP.Requests
+{
+    public class HttpRequest : IHttpRequest
+    {
+        public HttpRequest(string requestString)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(requestString, nameof(requestString));
+
+            this.FormData = new Dictionary<string, object>();
+            this.QueryData = new Dictionary<string, object>();
+            this.Headers = new HttpHeaderCollection();
+
+            this.ParseRequest(requestString);
+        }
+
+        public string Path { get; private set; }
+
+        public string Url { get; private set; }
+
+        public Dictionary<string, object> FormData { get; }
+
+        public Dictionary<string, object> QueryData { get; }
+
+        public IHttpHeaderCollection Headers { get; }
+
+        public HttpRequestMethod RequestMethod { get; private set; }
+
+        private bool IsValidRequestLine(string[] requestLine)
+        {
+            if (requestLine.Length != 3 || requestLine[2] != GlobalConstants.HttpOneProtocolFragment)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsValidRequestQueryString(string queryString, string[] queryParameters)
+        {
+            return true;
+        }
+
+        private void ParseRequestMethod(string[] requestLine)
+        {
+            HttpRequestMethod method;
+            bool isParsed = HttpRequestMethod.TryParse(requestLine[0], out method);
+
+            if (!isParsed)
+            {
+                throw new BadRequestException(string.Format(GlobalConstants.UnsupportedHttpMethodExceptionMessage, requestLine[0]));
+            }
+
+            this.RequestMethod = method;
+        }
+
+        private void ParseRequestUrl(string[] requestLine)
+        {
+            this.Url = requestLine[1];
+        }
+
+        private void ParseRequestPath()
+        {
+            this.Path = this.Url.Split('?')[0];
+        }
+
+        private void ParseHeaders(string[] plainHeaders)
+        {
+            plainHeaders.Select(plainHeader => plainHeader.Split(new[] { ':', ' ' }
+                   , StringSplitOptions.RemoveEmptyEntries))
+                   .ToList()
+                   .ForEach(headerKeyValuePair => this.Headers.AddHeader(new HttpHeader(headerKeyValuePair[0], headerKeyValuePair[1])));
+        }
+
+        private void ParseCookies()
+        {
+
+        }
+
+        private void ParseRequestQueryParameters()
+        {
+            this.Url.Split('?')[1]
+                .Split('&')
+                .Select(plainQueryParameter => plainQueryParameter.Split('='))
+                .ToList()
+                .ForEach(queryParameterKeyValuePair => this.QueryData.Add(queryParameterKeyValuePair[0], queryParameterKeyValuePair[1]));
+        }
+
+        private void ParseRequestFormDataParameters(string formData)
+        {
+            //TO DO Prase Multiple Parameters By Name
+            formData
+                .Split('&')
+                .Select(plainQueryParameter => plainQueryParameter.Split('='))
+                .ToList()
+                .ForEach(queryParameterKeyValuePair => this.FormData.Add(queryParameterKeyValuePair[0], queryParameterKeyValuePair[1]));
+        }
+
+        private void ParseRequestParameters(string formData)
+        {
+            this.ParseRequestQueryParameters();
+            this.ParseRequestFormDataParameters(formData);
+        }
+
+        private void ParseRequest(string requestString)
+        {
+            string[] splitRequestContent = requestString
+                .Split(new[] { GlobalConstants.HttpNewLine }, StringSplitOptions.None);
+
+            string[] requestLine = splitRequestContent[0].Trim()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (!this.IsValidRequestLine(requestLine))
+            {
+                throw new BadRequestException();
+            }
+
+            this.ParseRequestMethod(requestLine);
+            this.ParseRequestUrl(requestLine);
+            this.ParseRequestPath();
+
+            this.ParseHeaders(splitRequestContent.Skip(1).ToArray());
+            this.ParseCookies();
+
+            this.ParseRequestParameters(splitRequestContent[splitRequestContent.Length - 1]);
+        }
+    }
+}
