cffb15df371188c13d56fd2e13665294d8741730
deedeedextor <zavinagi_15@abv.bg>
2020-01-01 23:17:49 +0200

    Implemented cookies class

---
+++ C# Web/Web Basics/Http Protocol/SIS/SIS/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,59 @@
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Cookies.Contracts;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SIS.HTTP.Cookies
+{
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private const string HttpCookiesStringSeparator = " ";
+        private Dictionary<string, HttpCookie> cookies;
+
+        public HttpCookieCollection()
+        {
+            this.cookies = new Dictionary<string, HttpCookie>();
+        }
+
+        public void AddCookie(HttpCookie cookie)
+        {
+            CoreValidator.ThrowIfNull(cookie, nameof(cookie));
+
+            this.cookies.Add(cookie.Key, cookie);
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.cookies.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.cookies[key];
+        }
+
+        public IEnumerator<HttpCookie> GetEnumerator()
+        {
+            return this.cookies.Values.GetEnumerator();
+        }
+
+        public bool HasCookies()
+        {
+            return this.cookies.Count != 0;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(HttpCookiesStringSeparator, this.cookies.Values);
+        }
+    }
+}
