69fa816646236294612e9656e961923f28b38bac
deedeedextor <zavinagi_15@abv.bg>
2020-01-08 16:36:47 +0200

    Changed IRunes.App: *Implemented AlbumController *Modified Albums' All and Details views *Added EntityExtensions class, which contains methods returning Html result

--- C# Web/Web Basics/Workshop - Bootstrap/04. CSharp-Web-Basics-Workshop-Web-Application-Advanced-CSS-Bootstrap-Skeleton/IRunes.App/Controllers/AlbumsController.cs
+++ C# Web/Web Basics/Workshop - Bootstrap/04. CSharp-Web-Basics-Workshop-Web-Application-Advanced-CSS-Bootstrap-Skeleton/IRunes.App/Controllers/AlbumsController.cs
@@ -1,7 +1,102 @@
-﻿namespace IRunes.App.Controllers
+﻿using IRunes.App.Extensions;
+using IRunes.Data;
+using IRunes.Models;
+using SIS.HTTP.Requests.Contracts;
+using SIS.HTTP.Responses.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IRunes.App.Controllers
 {
     public class AlbumsController : BaseController
     {
+        public IHttpResponse All(IHttpRequest  httpRequest)
+        {
+            if (!this.IsLoggedIn(httpRequest))
+            {
+                return this.Redirect("/Users/Login");
+            }
 
+            using (var context = new RunesDbContext())
+            {
+                ICollection<Album> allAlbums = context.Albums.ToList();
+
+                if (allAlbums.Count == 0)
+                {
+                    this.ViewData["Albums"] = "There are currently no albums.";
+                }
+
+                else
+                {
+                    this.ViewData["Albums"] =
+                        string.Join("<br />", allAlbums
+                    .Select(album => album.ToHtmlAll()).ToList());
+                }
+            }
+
+            return this.View();
+        }
+
+        public IHttpResponse Create(IHttpRequest httpRequest)
+        {
+            if (!this.IsLoggedIn(httpRequest))
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            return this.View();
+        }
+
+        public IHttpResponse CreateConfirm(IHttpRequest httpRequest)
+        {
+            if (!this.IsLoggedIn(httpRequest))
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            using (var context = new RunesDbContext())
+            {
+                string name = ((ISet<string>)httpRequest.FormData["name"]).FirstOrDefault();
+                string cover = ((ISet<string>)httpRequest.FormData["cover"]).FirstOrDefault();
+
+                var album = new Album
+                {
+                    Name = name,
+                    Cover = cover,
+                    Price = 0M,
+                };
+
+                context.Albums.Add(album);
+                context.SaveChanges();
+            }
+
+            return this.Redirect("/Albums/All");
+        }
+
+        public IHttpResponse Details(IHttpRequest httpRequest)
+        {
+            if (!this.IsLoggedIn(httpRequest))
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            string albumId = httpRequest.QueryData["id"].ToString();
+
+            using (var context = new RunesDbContext())
+            {
+                var albumFromContext = context
+                    .Albums
+                    .SingleOrDefault(album => album.Id == albumId);
+
+                if (albumFromContext == null)
+                {
+                    return this.Redirect("/Albums/All");
+                }
+
+                this.ViewData["Album"] = albumFromContext.ToHtmlDetails();
+            }
+
+            return this.View();
+        }
     }
 }
--- C# Web/Web Basics/Workshop - Bootstrap/04. CSharp-Web-Basics-Workshop-Web-Application-Advanced-CSS-Bootstrap-Skeleton/IRunes.App/Launcher.cs
+++ C# Web/Web Basics/Workshop - Bootstrap/04. CSharp-Web-Basics-Workshop-Web-Application-Advanced-CSS-Bootstrap-Skeleton/IRunes.App/Launcher.cs
@@ -38,6 +38,13 @@
             serverRoutingTable.Add(HttpRequestMethod.Post, "/Users/Register", request => new UsersController().RegisterConfirm(request));
             serverRoutingTable.Add(HttpRequestMethod.Get, "/Users/Logout", request => new UsersController().Logout(request));
             #endregion
+
+            #region Albums Routes
+            serverRoutingTable.Add(HttpRequestMethod.Get, "/Albums/All", request => new AlbumsController().All(request));
+            serverRoutingTable.Add(HttpRequestMethod.Get, "/Albums/Create", request => new AlbumsController().Create(request));
+            serverRoutingTable.Add(HttpRequestMethod.Post, "/Albums/Create", request => new AlbumsController().CreateConfirm(request));
+            serverRoutingTable.Add(HttpRequestMethod.Get, "/Albums/Details", request => new AlbumsController().Details(request));
+            #endregion
         }
     }
 }
