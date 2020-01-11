3989195489bc4e7183d287baaf9a188350a7b89c
deedeedextor <zavinagi_15@abv.bg>
2020-01-11 12:12:11 +0200

    Remove project IRunes

--- C# Web/Web Basics/Workshop - Bootstrap/04. CSharp-Web-Basics-Workshop-Web-Application-Advanced-CSS-Bootstrap-Skeleton/IRunes.Data/DatabaseConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IRunes.Data
-{
-    public class DatabaseConfiguration
-    {
-        public const string ConnectionString = @"Server=.\SQLExpress;Database=IRunesDb; Integrated Security=true;";
-    }
-}
