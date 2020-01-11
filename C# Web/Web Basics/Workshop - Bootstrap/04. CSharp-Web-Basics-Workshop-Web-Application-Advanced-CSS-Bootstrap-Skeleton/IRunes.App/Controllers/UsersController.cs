72e6454c907426434c43d3bdec9ad47642cb0b79
deedeedextor <zavinagi_15@abv.bg>
2020-01-11 15:14:18 +0200

    Added IRunes project

---
+++ C# Web/Web Basics/Workshop - Bootstrap/04. CSharp-Web-Basics-Workshop-Web-Application-Advanced-CSS-Bootstrap-Skeleton/IRunes.App/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using IRunes.Data;
+using IRunes.Models;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IRunes.App.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                return Encoding.UTF8.GetString(sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password)));
+            }
+        }
+
+        public IHttpResponse Login(IHttpRequest httpRequest)
+        {
+            return this.View();
+        }
+
+        public IHttpResponse LoginConfirm(IHttpRequest httpRequest)
+        {
+            using (var context = new RunesDbContext())
+            {
+                string username = ((ISet<string>)httpRequest.FormData["username"]).FirstOrDefault();
+                string password = ((ISet<string>)httpRequest.FormData["password"]).FirstOrDefault();
+
+                if (username == null || password == null)
+                {
+                    return Redirect("/Users/Login");
+                }
+
+                var userFromContext = context.Users.FirstOrDefault(user => (user.Username == username || user.Email == username) && user.Password == this.HashPassword(password));
+
+                if (userFromContext == null)
+                {
+                    return this.Redirect("/Users/Login");
+                }
+
+                this.SignIn(httpRequest, userFromContext);
+            }
+
+            return this.Redirect("/");
+        }
+
+        public IHttpResponse Register(IHttpRequest httpRequest)
+        {
+            return this.View();
+        }
+
+        public IHttpResponse RegisterConfirm(IHttpRequest httpRequest)
+        {
+            using (var context = new RunesDbContext())
+            {
+                string username = ((ISet<string>)httpRequest.FormData["username"]).FirstOrDefault();
+                string password = ((ISet<string>)httpRequest.FormData["password"]).FirstOrDefault();
+                string confirmPassword = ((ISet<string>)httpRequest.FormData["confirmPassword"]).FirstOrDefault();
+                string email = ((ISet<string>)httpRequest.FormData["email"]).FirstOrDefault();
+
+                if (password != confirmPassword)
+                {
+                    return Redirect("/Users/Register");
+                }
+
+                var user = new User
+                {
+                    Username = username,
+                    Password = this.HashPassword(password),
+                    Email = email,
+                };
+
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+
+            return Redirect("/Users/Login");
+        }
+
+        public IHttpResponse Logout(IHttpRequest httpRequest)
+        {
+            this.SignOut(httpRequest);
+
+            return this.Redirect("/");
+        }
+    }
+}
