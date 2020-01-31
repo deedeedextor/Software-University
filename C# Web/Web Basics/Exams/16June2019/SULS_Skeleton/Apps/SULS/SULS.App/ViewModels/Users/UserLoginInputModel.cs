a4136a6fcef0147d6e58a62db60109e7f78dbabc
deedeedextor <zavinagi_15@abv.bg>
2020-01-31 16:52:07 +0200

    Add UserLoginInputModel and UserRegisterInputModel

---
+++ C# Web/Web Basics/Exams/16June2019/SULS_Skeleton/Apps/SULS/SULS.App/ViewModels/Users/UserLoginInputModel.cs
@@ -0,0 +1,15 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+
+namespace SULS.App.ViewModels.Users
+{
+    public class UserLoginInputModel
+    {
+        [RequiredSis]
+        [StringLengthSis(5, 20, "Username' length must be between 5 and 20!")]
+        public string Username { get; set; }
+
+        [RequiredSis]
+        [StringLengthSis(6, 20, "Password's lengthmust be between 6 and 20!")]
+        public string Password { get; set; }
+    }
+}
---
+++ C# Web/Web Basics/Exams/16June2019/SULS_Skeleton/Apps/SULS/SULS.App/ViewModels/Users/UserRegisterInputModel.cs
@@ -0,0 +1,23 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+
+namespace SULS.App.ViewModels.Users
+{
+    public class UserRegisterInputModel
+    {
+        [RequiredSis]
+        [StringLengthSis(5, 20, "Username's length must be between 5 and 20!")]
+        public string Username { get; set; }
+
+        [RequiredSis]
+        [EmailSis]
+        public string Email { get; set; }
+
+        [RequiredSis]
+        [StringLengthSis(6, 20, "Password's length must be between 6 and 20!")]
+        public string Password { get; set; }
+
+        [RequiredSis]
+        [StringLengthSis(6, 20, "Password's length must be between 6 and 20!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
