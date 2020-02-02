01755eddf00a4f8aabc1d0dad263d47916c31003
deedeedextor <zavinagi_15@abv.bg>
2020-02-02 12:38:28 +0200

    Refactor HomeController to show All Problems

--- C# Web/Web Basics/Exams/16June2019/SULS_Skeleton/Apps/SULS/SULS.App/Controllers/HomeController.cs
+++ C# Web/Web Basics/Exams/16June2019/SULS_Skeleton/Apps/SULS/SULS.App/Controllers/HomeController.cs
@@ -1,20 +1,51 @@
 ﻿using SIS.MvcFramework;
 using SIS.MvcFramework.Attributes;
 using SIS.MvcFramework.Result;
+using SULS.App.ViewModels.Home;
+using SULS.Services;
+using System;
+using System.Linq;
 
 namespace SULS.App.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProblemService problemService;
+        private readonly ISubmissionService submissionService;
+
+        public HomeController(IProblemService problemService, ISubmissionService submissionService)
+        {
+            this.problemService = problemService;
+            this.submissionService = submissionService;
+        }
+
         [HttpGet(Url = "/")]
+        public IActionResult IndexSlash()
+        {
+            return this.Index();
+        }
+
         public IActionResult Index()
         {
-            if (!IsLoggedIn())
+            if (IsLoggedIn())
             {
-                return this.View();
+                var problems = this.problemService
+                    .GetAll();
+
+                var result = problems
+                    .Select(p => new IndexViewModel()
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Count = this.submissionService.GetSubmissionsCountById(p.Id)
+                    })
+                    .ToList();
+
+                return this.View(result, view: "IndexLoggedIn");
             }
 
-            return this.View("IndexLoggedIn");
+            return this.View();
+
         }
     }
 }
