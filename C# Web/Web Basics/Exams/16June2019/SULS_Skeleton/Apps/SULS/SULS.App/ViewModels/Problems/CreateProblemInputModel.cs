1b80a817efc7ab759e66c9dc23e7f01aa5fcae17
deedeedextor <zavinagi_15@abv.bg>
2020-02-02 18:23:20 +0200

    Add problem and submission view models

---
+++ C# Web/Web Basics/Exams/16June2019/SULS_Skeleton/Apps/SULS/SULS.App/ViewModels/Problems/CreateProblemInputModel.cs
@@ -0,0 +1,18 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+
+namespace SULS.App.ViewModels.Problems
+{
+    public class CreateProblemInputModel
+    {
+        private const string ProblemErrorMessage = "Username length must be between 5 and 20!";
+        private const string PointsErrorMessage = "Points must be between 50 and 300!";
+
+        [RequiredSis]
+        [StringLengthSis(5, 20, ProblemErrorMessage)]
+        public string Name { get; set; }
+
+        [RequiredSis]
+        [RangeSis(50, 300, PointsErrorMessage)]
+        public int Points { get; set; }
+    }
+}
---
+++ C# Web/Web Basics/Exams/16June2019/SULS_Skeleton/Apps/SULS/SULS.App/ViewModels/Submissions/DetailsSubmissionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SULS.App.ViewModels.Submissions
+{
+    public class DetailsSubmissionViewModel
+    {
+        private int maxPoints;
+        private int achievedResult;
+
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public int FinalPoints { get; set; }
+
+        public int AchievedResult
+        {
+            get => int.Parse(Math.Round((this.achievedResult / (double)this.maxPoints) * 100).ToString());
+            set => this.maxPoints = value;
+        }
+
+        public int MaxPoints
+        {
+            get => this.MaxPoints;
+            set => this.maxPoints = value;
+        }
+
+        public string CreatedOn { get; set; }
+    }
+}
