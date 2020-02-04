38a600b1be9cdf9f8069e73c03a8b8906fa17e6b
deedeedextor <zavinagi_15@abv.bg>
2020-02-04 15:32:54 +0200

    Add MUSACA.Models

---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/MUSACA.Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using MUSACA.Models.Enums;
+
+namespace MUSACA.Models
+{
+    public class Order
+    {
+        public string Id { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Active;
+
+        public string ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string CashierId { get; set; }
+
+        public User Cashier { get; set; }
+    }
+}
---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/MUSACA.Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MUSACA.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Orders = new HashSet<Order>();
+        }
+
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
