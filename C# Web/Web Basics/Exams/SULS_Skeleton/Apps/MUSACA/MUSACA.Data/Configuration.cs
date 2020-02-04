d2729b50ac7b6e7a488f5da831aefc1a4fa8d8d6
deedeedextor <zavinagi_15@abv.bg>
2020-02-04 15:37:13 +0200

    Remove project

--- C# Web/Web Basics/Exams/SULS_Skeleton/Apps/MUSACA/MUSACA.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MUSACA.Data
-{
-    public class Configuration
-    {
-        public const string ConnestionString = @"Server=.\SQLExpress;Database=SulsDb;Integrated Security=true;";
-    }
-}
