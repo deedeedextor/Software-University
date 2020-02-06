a7072ebd78bdbb18c2e264654875dbeb930fb586
deedeedextor <zavinagi_15@abv.bg>
2020-02-06 12:35:43 +0200

    Add controllers

---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/MUSACA/MUSACA.App/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using MUSACA.App.ViewModels.Orders;
+using MUSACA.Services;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Result;
+using SIS.MvcFramework.Mapping;
+using MUSACA.App.ViewModels.Products;
+
+namespace MUSACA.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOrderService orderService;
+
+        public HomeController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        [HttpGet(Url = "/")]
+        public IActionResult IndexSlash()
+        {
+            return this.Index();
+        }
+
+        public IActionResult Index()
+        {
+            OrderHomeViewModel orderHomeViewModel = new OrderHomeViewModel();
+
+            if (this.IsLoggedIn())
+            {
+                var order = this.orderService
+                    .GetCurrentActiveOrderByCashierId(this.User.Id);
+
+                orderHomeViewModel = order.To<OrderHomeViewModel>();
+
+                orderHomeViewModel.Products.Clear();
+
+                foreach (var orderProduct in order.Products)
+                {
+                    ProductHomeViewModel productHomeViewModel = orderProduct.To<ProductHomeViewModel>();
+                    orderHomeViewModel.Products.Add(productHomeViewModel);
+                }
+            }
+
+            return this.View(orderHomeViewModel);
+        }
+    }
+}
---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/MUSACA/MUSACA.App/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using MUSACA.App.ViewModels.Products;
+using MUSACA.Models;
+using MUSACA.Services;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Security;
+using SIS.MvcFramework.Mapping;
+using SIS.MvcFramework.Result;
+using System.Linq;
+
+namespace MUSACA.App.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [Authorize]
+        public IActionResult All()
+        {
+            var products = this.productService.GetAll()
+                .Select(p => new ProductAllViewModel
+                {
+                    Name = p.Name,
+                    Price = p.Price.ToString(),
+                })
+                .ToList();
+
+            return this.View(products);
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Create(ProductCreateInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            var product = new Product
+            {
+                Name = model.Name,
+                Price = decimal.Parse(model.Price),
+            };
+
+            this.productService.CreateProduct(product);
+
+            return this.Redirect("/Products/All");
+        }
+    }
+}
