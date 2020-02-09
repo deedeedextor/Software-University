583f131427477490fa24433539ae6190fb01d7ca
deedeedextor <zavinagi_15@abv.bg>
2020-02-09 17:33:57 +0200

    Add models to PANDA.Models project

---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/PANDA/PANDA.Models/Package.cs
@@ -0,0 +1,33 @@
+﻿using PANDA.Models.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PANDA.Models
+{
+    public class Package
+    {
+        public Package()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Description { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        public PackageStatus Status { get; set; } = PackageStatus.Pending;
+
+        public DateTime EstimatedDeliveryDate { get; set; }
+
+        [Required]
+        public string RecepientId { get; set; }
+
+        public virtual User Recipient { get; set; }
+    }
+}
---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/PANDA/PANDA.Models/Receipt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PANDA.Models
+{
+    public class Receipt
+    {
+        public Receipt()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        public decimal Fee { get; set; }
+
+        public DateTime IssuedOn { get; set; }
+
+        [Required, ForeignKey(nameof(User))]
+        public string RecipientId { get; set; }
+
+        public virtual User Recipient { get; set; }
+
+        [Required, ForeignKey(nameof(Package))]
+        public string PackageId { get; set; }
+
+        public virtual Package Package { get; set; }
+    }
+}
