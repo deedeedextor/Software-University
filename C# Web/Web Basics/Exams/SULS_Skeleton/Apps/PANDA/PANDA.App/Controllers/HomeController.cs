ccde96572c7cbedcc7361c27f436ece73c23c02a
deedeedextor <zavinagi_15@abv.bg>
2020-02-10 15:50:37 +0200

    Implement home and user controllers

---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/PANDA/PANDA.App/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Result;
+using SIS.MvcFramework.Mapping;
+using PANDA.Services;
+
+namespace MUSACA.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet(Url = "/")]
+        public IActionResult IndexSlash()
+        {
+            return this.Index();
+        }
+
+        public IActionResult Index()
+        {
+            if (this.IsLoggedIn())
+            {
+                return this.View("IndexLoggedIn");
+            }
+
+            return this.View();
+        }
+    }
+}
---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/PANDA/PANDA.App/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Security;
+using SIS.MvcFramework.Result;
+using PANDA.Services;
+using PANDA.App.ViewModels.Users;
+
+namespace PANDA.App.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUsersService userService;
+
+        public UsersController(IUsersService userService)
+        {
+            this.userService = userService;
+        }
+
+        public IActionResult Login()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserLoginInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var userFromDb = this.userService
+                .GetUserByUsernameAndPassword(model.Username, model.Password);
+
+            if (userFromDb == null)
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            this.SignIn(userFromDb.Id, userFromDb.Username, userFromDb.Email);
+
+            return this.Redirect("/");
+        }
+
+        public IActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserRegisterInputModel model)
+        {
+            if (!ModelState.IsValid || model.Password != model.ConfirmPassword)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            this.userService.CreateUser(model.Username, model.Email, model.Password);
+
+            return this.Redirect("/Users/Login");
+        }
+
+        [Authorize]
+        public IActionResult Logout()
+        {
+            this.SignOut();
+
+            return this.Redirect("/");
+        }
+    }
+}
