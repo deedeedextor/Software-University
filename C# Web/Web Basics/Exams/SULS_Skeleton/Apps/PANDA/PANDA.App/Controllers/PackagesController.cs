8a755a0c1b71595d9dcbad6435e3f47e31cb5797
deedeedextor <zavinagi_15@abv.bg>
2020-02-11 11:50:32 +0200

    Implement Packages and Receipts controllers

---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/PANDA/PANDA.App/Controllers/PackagesController.cs
@@ -0,0 +1,85 @@
+﻿using PANDA.App.ViewModels.Packages;
+using PANDA.Models.Enums;
+using PANDA.Services;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Security;
+using SIS.MvcFramework.Result;
+using System.Linq;
+
+namespace PANDA.App.Controllers
+{
+    public class PackagesController : Controller
+    {
+        private readonly IPackagesService packagesService;
+        private readonly IUsersService usersService;
+
+        public PackagesController(IPackagesService packageService, IUsersService userService)
+        {
+            this.packagesService = packageService;
+            this.usersService = userService;
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            var users = this.usersService.GetUsernames();
+
+            return this.View(users);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Create(PackageCreateInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.Redirect("/Packages/Create");
+            }
+
+            this.packagesService.CreatePackage(model.Description, model.Weight, model.ShippingAddress, model.RecipientName);
+
+            return this.Redirect("/Packages/Pending");
+        }
+
+        [Authorize]
+        public IActionResult Pending()
+        {
+            var packages = this.packagesService.GetAllPackagesByStatus(PackageStatus.Pending)
+                .Select(p => new PackageViewModel
+                {
+                    Description = p.Description,
+                    Id = p.Id,
+                    Weight = p.Weight,
+                    ShippingAddress = p.ShippingAddress,
+                    RecipientName = p.Recipient.Username,
+                }).ToList();
+
+            return this.View(new PackagesListViewModel { Packages = packages});
+        }
+
+        [Authorize]
+        public IActionResult Delivered()
+        {
+            var packages = this.packagesService.GetAllPackagesByStatus(PackageStatus.Delivered)
+                 .Select(p => new PackageViewModel
+                 {
+                     Description = p.Description,
+                     Id = p.Id,
+                     Weight = p.Weight,
+                     ShippingAddress = p.ShippingAddress,
+                     RecipientName = p.Recipient.Username,
+                 }).ToList();
+
+            return this.View(new PackagesListViewModel { Packages = packages });
+        }
+
+        [Authorize]
+        public IActionResult Deliver(string id)
+        {
+            this.packagesService.Deliver(id);
+
+            return this.Redirect("/Packages/Delivered");
+        }
+    }
+}
---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/PANDA/PANDA.App/Controllers/ReceiptsController.cs
@@ -0,0 +1,34 @@
+﻿using PANDA.App.ViewModels.Receipts;
+using PANDA.Services;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes.Security;
+using SIS.MvcFramework.Result;
+using System.Linq;
+
+namespace PANDA.App.Controllers
+{
+    public class ReceiptsController : Controller
+    {
+        private readonly IReceiptsService receiptsService;
+
+        public ReceiptsController(IReceiptsService receiptsService)
+        {
+            this.receiptsService = receiptsService;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            var receipts = this.receiptsService.GetAll()
+                .Select(r => new ReceiptViewModel
+                {
+                    Id = r.Id,
+                    Fee = r.Fee,
+                    IssuedOn = r.IssuedOn,
+                    RecipientName = r.Recipient.Username,
+                }).ToList();
+
+            return this.View(receipts);
+        }
+    }
+}
