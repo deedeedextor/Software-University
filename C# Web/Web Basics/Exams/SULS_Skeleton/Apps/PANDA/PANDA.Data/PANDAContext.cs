0fbdfc1a8cf70478ecf322c76672ce176c24932a
deedeedextor <zavinagi_15@abv.bg>
2020-02-11 13:11:33 +0200

    Add PANDAContext

---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/PANDA/PANDA.Data/PANDAContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PANDA.Models;
+
+namespace PANDA.Data
+{
+    public class PANDAContext : DbContext
+    {
+        public PANDAContext()
+        {
+        }
+
+        public PANDAContext(DbContextOptions<PANDAContext> options) : base()
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Package> Packages { get; set; }
+
+        public DbSet<Receipt> Receipts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.ConnestionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                   .HasKey(user => user.Id);
+
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.Packages)
+                .WithOne(x => x.Recipient)
+                .HasForeignKey(x => x.RecepientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.Receipts)
+                .WithOne(x => x.Recipient)
+                .HasForeignKey(x => x.RecipientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
