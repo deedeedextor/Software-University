87c7f1eb562587b1861e48d37f269783f7a40fa8
deedeedextor <zavinagi_15@abv.bg>
2020-02-15 17:18:59 +0200

    Refactor Problems and Submission view models in SULS.App

---
+++ C# Web/Web Basics/Exams/SULS_Skeleton/Apps/SULS/SULS.App/ViewModels/Problems/DetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SULS.App.ViewModels.Submissions;
+using System.Collections.Generic;
+
+namespace SULS.App.ViewModels.Problems
+{
+    public class DetailsViewModel
+    {
+        public DetailsViewModel()
+        {
+            this.Problems = new List<ProblemDetailsSubmissionViewModel>();
+        }
+
+        public string Name { get; set; }
+
+        public IEnumerable<ProblemDetailsSubmissionViewModel> Problems { get; set; }
+    }
+}
